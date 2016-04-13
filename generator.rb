37990425821168b1bd33c4e0150e877a346b5e52
ivan.skiridomov <ivan.skiridomov@exness.com>
2016-04-13 11:40:58 +0300

    add random hostname

--- generator.rb
+++ generator.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
+require 'digest/sha1'
 
 get '/cloud-config' do
-  erb :config, :locals => {:ip => params[:ip]}
+
+  erb :config, :locals => {:ip => params[:ip],
+                           :digest => Digest::SHA1.hexdigest(params[:ip].to_s) }
 end
